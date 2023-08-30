13e50149a227ac17c56575f1ffba79960f43feeb
Polina Gorelik <Polina23707@gmail.com>
2023-08-30 15:37:50 +0300

    Восьмое задание готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  const maxCacheValuesCount = 5;
+  return (...args) => {
+    const hash = args.join(',');
+    for(let i = 0; cache.length > i; i ++){
+      if(cache[i].hash === hash){
+      return "Из кеша: " + cache[i].value; 
+      }
+    }
+    
+    const result = func(...args);
+    cache.push({hash: hash,value: result});
+    const actualCacheValuesCount = Object.keys(cache).length;
+
+    if(actualCacheValuesCount > maxCacheValuesCount){
+      cache.shift();
+    }
+
+    return "Вычисляем: " + result;
+  }
 }
+
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    wrapper.allCount += 1;
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count += 1;
+    }
+    
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count += 1;
+      }, delay);
+  }
   
+  return wrapper;
 }
+
